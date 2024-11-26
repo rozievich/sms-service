e1d79309e6c938d8d64654e59504e0f65639f766
rozievich <oybekrozievich@gmail.com>
2024-11-26 17:13:32 +0500

    Fixed bugs and optimizing code

--- sms/views.py
+++ sms/views.py
@@ -8,7 +8,6 @@ from rest_framework.viewsets import ModelViewSet
 from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.permissions import IsAuthenticated
-from rest_framework.parsers import JSONParser
 
 from config.settings import SMS_FROM, SMS_EMAIL, SMS_API_KEY
 from .models import Message, CustomUser, GlobalMessage
